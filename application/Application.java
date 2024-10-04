a7f0df3d10fe2fd8a11ce4d7297562ec835ffe5f
Eduardo Viana <23eduardoviana@gmail.com>
2024-10-03 22:39:52 -0300

    Adcionado a aplicação que não foi enviada no original

--- application/Application.java
+++ application/Application.java
@@ -1,5 +1,45 @@
 package application;
 
+
+import java.util.Scanner;
+
+import entities.Triangle;
+
 public class Application {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		Triangle x, y;
+		x = new Triangle();
+		y = new Triangle();
+		
+		System.out.println("Enter the measures of triangle X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		
+		System.out.println("Enter the measures of triangle Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+	
+		double areaX = x.area();
+		
+		
+		double areaY = y.area();
+		
+		System.out.printf("Triangle X area: %.4f%n", areaX);
+		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		
+		if (areaX > areaY) {
+			System.out.println("Triangle X is bigger");
+		} else {
+			System.out.println("Triangle Y is bigger");
+		}
+		
+		sc.close();
+	}
 }
+
